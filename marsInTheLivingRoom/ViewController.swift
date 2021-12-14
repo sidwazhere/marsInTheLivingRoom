e94ab47d01f8dd43824d00ae6c5ad664456ef154
Sideeq <sideeqw@gmail.com>
2021-12-13 23:39:31 -0500

    Final

--- marsInTheLivingRoom/ViewController.swift
+++ marsInTheLivingRoom/ViewController.swift
@@ -13,20 +13,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let sphere = SCNSphere(radius: 0.5)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_mars.jpg")
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0, z: -1)
+        
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
     }
     
     override func viewWillAppear(_ animated: Bool) {
